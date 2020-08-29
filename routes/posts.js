ff8f59042a3a0ae36058ccc90bb6cb1dc525f0e2
Pratyush Vishal <pratyushvishal04@gmail.com>
2020-08-29 15:36:59 +0530

    add chatting box

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const postsController = require('../controllers/posts_controller');
+const postsController = require('../controllers/posts_controllers');
 
 router.post('/create', passport.checkAuthentication, postsController.create);
 router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
