e64af695556984b23653bb90c0995e3266a14a8f
Pratyush Vishal <pratyushvishal04@gmail.com>
2020-08-29 13:58:44 +0530

    add socket.io for chat and like,dislike,add friend button

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,41 +1,47 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-const { serializeUser } = require('passport');
 
 
-module.exports.home = function(req, res){
-    // console.log(req.cookies);
-    // res.cookie('user_id', 25);
 
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title: "Codeial | Home",
-    //         posts:  posts
-    //     });
-    // });
+module.exports.home = async function(req, res){
 
-    // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
+    try{
+        // CHANGE :: populate the likes of each post and comment
+        let posts = await Post.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            },
+            populate: {
+                path: 'likes'
+            }
+        }).populate('likes');
 
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "Codeial | Home",
-                posts:  posts,
-                all_users: users
-            });
+    
+        let users = await User.find({});
+
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users: users
         });
 
-       
-    })
-
+    }catch(err){
+        console.log('Error', err);
+        return;
+    }
+   
 }
 
 // module.exports.actionName = function(req, res){}
+
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+
+// posts.then()
--- routes/posts.js
+++ routes/posts.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const postsController = require('../controllers/posts_controllers');
+const postsController = require('../controllers/posts_controller');
 
-router.post('/create', passport.checkAuthentication, postsController.createPost);
-router.get('/destroy/:id',passport.checkAuthentication, postsController.destroy);
+router.post('/create', passport.checkAuthentication, postsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
 
 module.exports = router;
